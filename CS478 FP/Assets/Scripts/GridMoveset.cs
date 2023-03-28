e24d00f2c03462b7f5b35b86eab8e76711678af9
Brian <bjmartin4@eagles.usi.edu>
2023-03-28 16:59:52 -0500

    new stuff 3/29

--- CS478 FP/Assets/Scripts/GridMoveset.cs
+++ CS478 FP/Assets/Scripts/GridMoveset.cs
@@ -4,43 +4,29 @@ using UnityEngine;
 
 public class GridMoveset : MonoBehaviour
 {
-    private bool isMoving;
-    private Vector3 origPos, targetPos;
-    private float moveSpeed = 4f;
-    public LayerMask obstacleLayers;
-    public Transform movePoint;
-    //private float moveSpeed;
-    public Animator anim;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        movePoint.parent = null;
-    }
+    public float MoveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        ProcessInputs();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
-        {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
-            {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f), .2f, obstacleLayers))
-                {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f);
-                }
-            }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
-            {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical")), .2f, obstacleLayers))
-                {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"));
-                }
-            }
-        }      
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+    }
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * MoveSpeed, moveDirection.y * MoveSpeed);
     }
 }
