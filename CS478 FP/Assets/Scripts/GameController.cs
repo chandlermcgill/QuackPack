3f8c47d05dd288d3530e05294bdea126e31a1ae9
Ryan <rmschapker@eagles.usi.edu>
2023-04-23 22:32:28 -0500

    Battle-trans2
    small update to battle transition code

--- CS478 FP/Assets/Scripts/GameController.cs
+++ CS478 FP/Assets/Scripts/GameController.cs
@@ -15,6 +15,7 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         playerController.OnEncountered += StartBattle;
+        battleSystem.OnBattleOver += EndBattle;
     }
 
     void StartBattle()
@@ -22,6 +23,15 @@ public class GameController : MonoBehaviour
         state = ActiveGameState.Battle;
         battleSystem.gameObject.SetActive(true);
         worldCamera.gameObject.SetActive(false);
+
+        battleSystem.StartBattle();
+    }
+
+    void EndBattle(bool won)
+    {
+        state = ActiveGameState.FreeRoam;
+        battleSystem.gameObject.SetActive(false);
+        worldCamera.gameObject.SetActive(true);
     }
 
     private void Update()
